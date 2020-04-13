8918d32c123f3aded147a273c36f097cba0462dc
unclezap <zeb.blackwell@gmail.com>
2020-04-13 11:24:47 -0700

    adding quiz and browsing components

---
+++ src/API.js
@@ -0,0 +1,67 @@
+const API_ROOT = 'http://localhost:3000'
+//need to change when we go to heroku
+
+const token = () => localStorage.getItem("token")
+
+const headers = () => {
+    return {
+        "Content-Type":"application/json",
+        Accept: "application/json",
+        Authorization: token()
+    }
+}
+
+const getQuizzes = () => {
+    return fetch(`${API_ROOT}/quizzes`, {headers: headers()})
+    .then(response => response.json())
+}
+
+const getQuestions = () => {
+    //some sort of logic to determine which questions
+    return fetch(`${API_ROOT}/questions`, {headers: headers()})
+    .then(response => response.json())
+}
+//might have the answers come through questions in the serializer instead
+//leaving in for testing purposes now
+const getAnswers = () => {
+    return fetch(`${API_ROOT}/answers`, {headers: headers()})
+    .then(response => response.json())
+}
+
+const getUserScores = () => {
+    return fetch(`${API_ROOT}/userscores`, {headers: headers()})
+    .then(response => response.json())
+}
+
+const login = data => {
+  return fetch(`${API_ROOT}/auth`, {
+    method: "POST",
+    headers: headers(),
+    body: JSON.stringify(data)
+  }).then(res => res.json());
+};
+
+const getCurrentUser = () => {
+  // console.log("getting current user", headers);
+  return fetch(`${API_ROOT}/current_user`, {
+    headers: headers()
+  }).then(res => {
+    // console.log(res)
+    return res.json();
+  });
+};
+
+export const api = {
+  auth: {
+    login,
+    getCurrentUser
+  },
+  quizzes: {
+    getQuizzes,
+    getQuestions,
+    getAnswers,
+  },
+  scores: {
+      getUserScores
+  }
+};
--- src/containers/ShowPage.js
+++ src/containers/ShowPage.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Browse from '../components/Browse'
 
 class ShowPage extends Component {
     render(){
         return(
             <div>
                 {/* Details to come */}
+                {/* {add a ternary to show either the browse component or quiz component?} */}
+                <Browse />
+                
                 <h3>This is the showpage.</h3>
             </div>
         )
