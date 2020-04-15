ed81abbf1bf46fd7bc9fd2e60c8fa98c6f47bb58
javila35 <josepharthuravila@gmail.com>
2020-04-15 13:08:15 -0700

    Quiz now renders.

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { api } from '../api';
 import Question from './QuizComponents/Questions'
+import Submit from './QuizComponents/Submit';
 
 
 class Quiz extends Component {
@@ -38,6 +39,7 @@ class Quiz extends Component {
             return <Question 
             key={index}
             question_text={questions[question].text}
+            question_id={questions[question].id}
             answers={questions[question].answers}
             />
         }) 
@@ -60,6 +62,7 @@ class Quiz extends Component {
             <div>
                 {this.renderQuizInfo()}
                 {this.renderQuestions()}
+                <Submit />
             </div>
         )
     }
--- src/components/QuizComponents/Questions.js
+++ src/components/QuizComponents/Questions.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import Answer from './Answer';
 
 class Question extends Component {
 
+    renderAnswers = () =>{
+        const answers = this.props.answers
+        return Object.keys(answers).map((object, index) => {
+            return <Answer 
+            key={index}
+            answer={answers[object]}
+            />
+        })
+    }
 
     render() {
         return(
             <div>
-                <h1>{this.props.question_text}</h1>
+                <h3>{this.props.question_text}</h3>
+                <form>
+                    {this.renderAnswers()}
+                </form>
             </div>
-
         )
     }
 }
