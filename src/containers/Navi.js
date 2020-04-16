3a68a4f8914515a4a00214e5c2ab71923da02601
unclezap <zeb.blackwell@gmail.com>
2020-04-16 16:11:17 -0700

    popular search function

--- src/containers/Navi.js
+++ src/containers/Navi.js
@@ -12,6 +12,7 @@ function Navi(props) {
                 <Nav.Link><Link to="/">Home</Link></Nav.Link>
                 {localStorage.getItem("token") ? <Nav.Link><Link to="/account">My Account</Link></Nav.Link> : null}
                 <Nav.Link><Link to='/Browse'>Browse</Link></Nav.Link>
+                <Nav.Link><Link to='/Popular'>Popular</Link></Nav.Link>
                 <Nav.Link><Link to='/Leaderboard'>Leaderboard</Link></Nav.Link>
                 {localStorage.getItem("token")?null:<Nav.Link><Link to="/signup">Sign Up</Link></Nav.Link>}
                 {localStorage.getItem("token")?<Button variant="danger" onClick={props.onLogout}>Logout</Button>:<Form><LoginForm onAuthenticate={props.onAuthenticate}/></Form>}
