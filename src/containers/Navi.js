b946f189fc90dc9f0d89b308fc0d548076cd43f7
javila35 <josepharthuravila@gmail.com>
2020-04-15 22:01:11 -0700

    styled sign up a bit

--- src/containers/Navi.js
+++ src/containers/Navi.js
@@ -4,6 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 function Navi(props) {
     return(
@@ -13,7 +14,7 @@ function Navi(props) {
                 <Nav.Link><Link to='/Browse'>Browse</Link></Nav.Link>
                 <Nav.Link><Link to='/Leaderboard'>Leaderboard</Link></Nav.Link>
                 {localStorage.getItem("token")?null:<Nav.Link><Link to="/signup">Sign Up</Link></Nav.Link>}
-                {localStorage.getItem("token")?<button onClick={props.onLogout}>Logout</button>:<Form><LoginForm onAuthenticate={props.onAuthenticate}/></Form>}
+                {localStorage.getItem("token")?<Button variant="danger" onClick={props.onLogout}>Logout</Button>:<Form><LoginForm onAuthenticate={props.onAuthenticate}/></Form>}
             </Navbar>
      )
 };
--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { api } from '../api';
-import { Redirect } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
 
 class SignUp extends Component {
     constructor(){
@@ -42,45 +42,51 @@ class SignUp extends Component {
 
     render() {
         return(
-                <Form onSubmit={e => this.handleSubmit(e)}>
-                    <Form.Group >
-                        <Form.Label>
-                            Username:
-                        </Form.Label>
-                        <Form.Control 
-                            name="username" 
-                            placeholder="New Username" 
-                            onChange={this.handleChange}
-                            type="text" 
-                            value={this.state.fields.username}
-                             />
-                    </Form.Group>
-                    <Form.Group >
-                        <Form.Label>
-                            Password:
-                        </Form.Label>
-                        <Form.Control 
-                            name="password"
-                            onChange={this.handleChange}
-                            placeholder="Password" 
-                            type="password" 
-                            value={this.state.fields.password}
-                             />
-                    </Form.Group>
-                    <Form.Group >
-                        <Form.Label>
-                            Verify Password:
-                        </Form.Label>
-                        <Form.Control 
-                        name="verifyPassword"
-                        onChange={this.handleChange}
-                        placeholder="Password"
-                        value={this.state.fields.verifyPassword}
-                        type="password"
-                        />
-                    </Form.Group>
-                    <Button type="submit" variant="primary">Submit</Button>
-                </Form>
+            <Container>
+                <Row >
+                    <Col xs={5}>
+                        <Form onSubmit={e => this.handleSubmit(e)}>
+                            <Form.Group >
+                                <Form.Label>
+                                    Username:
+                                </Form.Label>
+                                <Form.Control 
+                                    name="username" 
+                                    placeholder="New Username" 
+                                    onChange={this.handleChange}
+                                    type="text" 
+                                    value={this.state.fields.username}
+                                    />
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>
+                                    Password:
+                                </Form.Label>
+                                <Form.Control 
+                                    name="password"
+                                    onChange={this.handleChange}
+                                    placeholder="Password" 
+                                    type="password" 
+                                    value={this.state.fields.password}
+                                    />
+                            </Form.Group>
+                            <Form.Group >
+                                <Form.Label>
+                                    Verify Password:
+                                </Form.Label>
+                                <Form.Control 
+                                name="verifyPassword"
+                                onChange={this.handleChange}
+                                placeholder="Password"
+                                value={this.state.fields.verifyPassword}
+                                type="password"
+                                />
+                            </Form.Group>
+                            <Button type="submit" variant="primary">Submit</Button>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
