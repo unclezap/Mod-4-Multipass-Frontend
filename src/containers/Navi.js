dc0e6af44af3a56cc0d405d3c19d3ccdb0e37050
unclezap <zeb.blackwell@gmail.com>
2020-04-17 09:59:51 -0700

    removed Create Quiz link in Navi

--- src/containers/Navi.js
+++ src/containers/Navi.js
@@ -10,11 +10,10 @@ function Navi(props) {
             <Navbar>
                 <Link className="nav-link" to="/">Home</Link>
                 {localStorage.getItem("token") ? <Link className="nav-link" to="/account">My Account</Link> : null}
-                {localStorage.getItem("token")? <Link className="nav-link" to="/new_quiz">Create Quiz</Link> : null}
+                {localStorage.getItem("token")? <Link className="nav-link" to="/new_quiz">New Quiz</Link> : null}
                 <Link className="nav-link" to='/Browse'>Browse</Link>
                 <Link className="nav-link" to='/Popular'>Popular</Link>
                 <Link className="nav-link" to='/Leaderboard'>Leaderboard</Link>
-                <Link className="nav-link" to='/new_quiz'>New Quiz</Link>
                 {localStorage.getItem("token")?null:<Link className="nav-link" to="/signup">Sign Up</Link>}
                 {localStorage.getItem("token")?<Button variant="outline-danger" onClick={props.onLogout}>Logout</Button>:<Form><LoginForm onAuthenticate={props.onAuthenticate}/></Form>}
             </Navbar>
