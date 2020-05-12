c15c576835392ac7dc1121f14e3332c4e3a91731
Agandaur <cjwagstaffe@gmail.com>
2020-05-12 14:06:01 -0700

    added in uuid

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -11,7 +11,7 @@ class Quiz extends Component {
             user: "",
             checkedAnswers: {}
         }
-    };
+    }; 
 
     componentDidMount() {
         this.getQuestions(this.props);
